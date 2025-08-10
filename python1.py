15db2f102f643b31e957551847081b783efe2043
adhalan123 <dhalangeaarati@gmail.com>
2025-08-10 17:29:26 +0530

    new playwrite python project practice 1

---
+++ python1.py
@@ -0,0 +1,26 @@
+# print hellow word
+
+print("Hello ,aaru")
+
+# print swap numbers
+
+a=10;  
+b=20;
+
+# now a=20 and b=10
+
+a=a+b; # a=30
+b=a-b; # b=10
+a= a-b; # 30-10=20 a=20
+
+print("a=",a,"b=",b)
+
+#check number is odd or even
+
+n=input("hi please enter number",)
+
+if (n%2==0):
+    
+    print(n,"=given number is even number")
+else:
+    print(n,"=given number is odd number")    
